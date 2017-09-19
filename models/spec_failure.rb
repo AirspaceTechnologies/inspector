fecef5b03ed183e4a32a71a84c297908a0d8dfcf
Chris Stump <chris@airspacetechnologies.com>
2017-09-18 20:37:06 -0500

    adds useful scopes to SpecFailure as well as #file_id

--- models/spec_failure.rb
+++ models/spec_failure.rb
@@ -1,9 +1,18 @@
 class SpecFailure < ActiveRecord::Base
   belongs_to :spec_run
+
   validates :description, :file_path, :spec_run_id, presence: true
+
+  scope :execution_expired, -> { where('exception @> ?', { message: 'execution expired' }.to_json) }
+
+  scope :flaky, -> { where.not('exception @> ?', { message: 'execution expired' }.to_json) }
 
   def self.create_from_example!(example, spec_run)
     %w(pending_message status).each{|key| example.delete(key) }        
     create!(example.merge(spec_run_id: spec_run.id))
+  end
+
+  def file_id
+    "#{file_path}:#{line_number}"
   end
 end
